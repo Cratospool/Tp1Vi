2c8f6e5cd0efb3d8faee79b9df01722845def328
Cratospool <luisjivillalba@gmail.com>
2019-03-31 12:38:07 -0300

    Prueva en GitHub

--- application/views/principal.php
+++ application/views/principal.php
@@ -6,6 +6,7 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -16,6 +17,9 @@
                 </div>
                 <div class="carousel-item">
                     <img src="assets/img/carousel-3.jpg" class="d-block w-100" alt="img3">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/carousel-4.jpg" class="d-block w-100" alt="img4">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -50,7 +54,7 @@
                         <img href="#" src="assets/img/card-2.jpg" class="card-img-top" alt="DarksidersII">
                         <div class="card-body">
                             <h5 class="card-title">Darksiders II PC Full Español</h5>
-                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 22 de Septiembre de </p>
+                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 5 de noviembre de 2015</p>
 
                         </div>
                     </div>
@@ -62,12 +66,46 @@
                         <img href="#" src="assets/img/card-3.jpg" class="card-img-top" alt="DarksidersIII">
                         <div class="card-body">
                             <h5 class="card-title">Darksiders III PC Full Español</h5>
-                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 22 de Septiembre de </p>
+                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 27 de Noviembre de 2018</p>
 
                         </div>
                     </div>
                 </div>
                 <!-- /Darksiders3 -->
+                <!-- Assassin's Creed IV: Black Flag -->
+                <div class="col mt-5 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-4.jpg" class="card-img-top" alt="Assassin’s Creed IV Black Flag">
+                        <div class="card-body">
+                            <h5 class="card-title">Assassin’s Creed IV Black Flag</h5>
+                            <p class="card-text">Acerca de Assassin’s Creed IV Black Flag PC <br>Título: Assassin’s Creed IV Black Flag <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 29 de octubre de 2013</p>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /Assassin's Creed IV: Black Flag -->
+                <!-- Sekiro: Shadows Die Twice -->
+                <div class="col mt-5 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-5.jpg" class="card-img-top" alt="Sekiro: Shadows Die Twice">
+                        <div class="card-body">
+                            <h5 class="card-title">Sekiro: Shadows Die Twice</h5>
+                            <p class="card-text">Título: Sekiro: Shadows Die Twice PC-GAME <br>Género: Acción, Aventura <br>Fecha de lanzamiento: 22 de Marzo de 2019 <br>Idioma Voces y textos: Español, Ingles, Francés, Alemán, Japones e Italiano. <br>Tamaño: 14.4 Gigas</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- /Sekiro: Shadows Die Twice -->
+                <!-- Devil May Cry 5  -->
+                <div class="col mt-5 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-6.jpg" class="card-img-top" alt="Devil May Cry 5 ">
+                        <div class="card-body">
+                            <h5 class="card-title">Devil May Cry 5 </h5>
+                            <p class="card-text">Título: Devil May Cry 5 PC-GAME <br>Género: Acción, Aventura <br>Fecha de lanzamiento: 8 de Marzo de 2019 <br>Idioma Voces y textos: Español, Ingles, Francés, Alemán, Japones e Italiano. <br>Tamaño: 35.4 Gigas</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- /Devil May Cry 5  -->
             </div>
             <!-- row -->
         </div>
