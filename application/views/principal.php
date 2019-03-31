1783fc7c1ed47132c4c4c62d38156f096378f160
Cratospool <luisjivillalba@gmail.com>
2019-03-30 23:40:41 -0300

    hasa ahi anda todo

---
+++ application/views/principal.php
@@ -0,0 +1,75 @@
+<body>
+    <main id="main">
+        <!-- carousel -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img/carousel-1.jpg" class="d-block w-100" alt="img1">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/carousel-2.jpg" class="d-block w-100" alt="img2">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/carousel-3.jpg" class="d-block w-100" alt="img3">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <!-- /carousel -->
+        <!-- cards -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <div class="col">
+                    <!-- Darksiders -->
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-1.jpg" class="card-img-top" alt="Darksiders">
+                        <div class="card-body">
+                            <h5 class="card-title">Darksiders PC Full Español</h5>
+                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 22 de Septiembre de </p>
+
+                        </div>
+                    </div>
+                    <!-- /Darksiders -->
+                </div>
+                <div class="col">
+                    <!-- Darksiders2 -->
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-2.jpg" class="card-img-top" alt="DarksidersII">
+                        <div class="card-body">
+                            <h5 class="card-title">Darksiders II PC Full Español</h5>
+                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 22 de Septiembre de </p>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /Darksiders2 -->
+                <!-- Darksiders3 -->
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img href="#" src="assets/img/card-3.jpg" class="card-img-top" alt="DarksidersIII">
+                        <div class="card-body">
+                            <h5 class="card-title">Darksiders III PC Full Español</h5>
+                            <p class="card-text">Acerca de Darksiders PC <br>Título: Darksiders PC Full Español <br>Género: Acción, Aventura <br>Plataforma: PC <br>Idioma: Francés | Ingles | Español | Alemán | Muchos Mas. <br>Fecha de lanzamiento: 22 de Septiembre de </p>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /Darksiders3 -->
+            </div>
+            <!-- row -->
+        </div>
+        <!-- /cards -->
+    </main>
