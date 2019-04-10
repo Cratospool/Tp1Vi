a40ad84e41b503a370eb70544634b270166859f0
Cratospool <luisjivillalba@gmail.com>
2019-04-10 00:01:41 -0300

    fondo metalico

--- application/views/contactanos.php
+++ application/views/contactanos.php
@@ -2,40 +2,27 @@
     <main id="main-contactanos">
         <div class="container">
             <div class="row">
-                <form>
-                      <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                      </div>
-                      <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                      </div>
-                      <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                      </div>
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-            <div class="row">
-                <select class="form-control form-control">
-                  <option>Elegí</option>
-                  <option>Consutas sobre tiempos de entrega</option>
-                  <option>Incumplimiento de la entrega</option>
-                  <option>Solicito envío de factura</option>
-                  <option>Mi factura tiene un error</option>
-                  <option>Incumplimeinto del servicio técnico</option>
-                  <option>Cambio autorizado por servicio téctnico</option>
-                  <option>Otros</option>
-                </select>
-
-            </div>
-            <div class="row">
-                <button data-tracking="shipping-deadlines" class="accordion">
-                    <h4>Cuales dson los plasos</h4>
-                </button>
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1"></label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1"></label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Seleccione un motivo</option>
+                                <option>1- Mi juego no fué entregado</option>
+                                <option>2- Solicitud de reembolso</option>
+                                <option>3- Vende tu juego a través de nuestra página</option>
+                                <option>4- Otro..</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1"></label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Mensaje"></textarea>
+                        </div>
+                                <button type="submit" class="btn btn-light">Enviar</button>
+                    </form>
             </div>
         </div>
     </main>
--- application/views/front/navbar_view.php
+++ application/views/front/navbar_view.php
@@ -1,50 +1,69 @@
-<header>
-    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark navbvar-static-top">
-        <a class="navbar-brand ">
-            <span class="navbar-logo">
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+
+            </div>
+            <div  class="col-md">
                 <a href="<?php echo base_url('principal');?>">
-                    <img id="logo" src="assets/img/logo.png" alt="">
+                    <img id="logo " src="assets/img/logo.png" alt="Logo" style="height: 200px;">
                 </a>
-            </span>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            </div>
+            <div class="col-4">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Productos<span class="sr-only">(current)</span></a>
-                </li>
-
-                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Categorias
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background: #323232;">
-                        <a class="dropdown-item" href="#">Acción</a>
-                        <a class="dropdown-item" href="#">Aventura</a>
-                        <a class="dropdown-item" href="#">Conducción</a>
-                        <a class="dropdown-item" href="#">Deportes</a>
-                        <a class="dropdown-item" href="#">Estrategia</a>
-                        <a class="dropdown-item" href="#">Lucha</a>
-                        <a class="dropdown-item" href="#">Simulacion</a>
-                        <a class="dropdown-item" href="#">Rol</a>
-                        <a class="dropdown-item" href="#">Terror</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Todos</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Carro</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
-                <button class="btn btn-outline-success my-2 my-sm-0"  type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-            </form>
+            </div>
         </div>
-    </nav>
+
+
+    </div>
+        <br>
+        <nav id="navbar" class="navbar navbar-expand-lg navbar-dark sticky-top" >
+            <a class="navbar-brand ">
+                <span class="navbar-logo">
+                    <a href="<?php echo base_url('principal');?>">Home</a>
+                </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Productos<span class="sr-only">(current)</span></a>
+                    </li>
+
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categorias
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background: #323232;">
+                            <a class="dropdown-item" href="#">Acción</a>
+                            <a class="dropdown-item" href="#">Aventura</a>
+                            <a class="dropdown-item" href="#">Conducción</a>
+                            <a class="dropdown-item" href="#">Deportes</a>
+                            <a class="dropdown-item" href="#">Estrategia</a>
+                            <a class="dropdown-item" href="#">Lucha</a>
+                            <a class="dropdown-item" href="#">Simulacion</a>
+                            <a class="dropdown-item" href="#">Rol</a>
+                            <a class="dropdown-item" href="#">Terror</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Todos</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carro</a>
+                    </li>
+                </ul>
+
+
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                    <button class="btn btn-outline-light my-2 my-sm-0"  type="submit">
+                      <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+        </nav>
+    </div>
 </header>
